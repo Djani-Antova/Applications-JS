f1630a938732dfbfda26e473fe70b8bac39e92d6
djaniAntova <djaniantova@gmail.com>
2023-04-22 17:02:10 +0300

    Create auth.js

---
+++ 11.Architecture-and-Testing-Lab/02. My-Cookbook-Architecture-Testing-Added/src/auth.js
@@ -0,0 +1,32 @@
+const guestNavigation = document.getElementById('guest');
+const userNavigation = document.getElementById('user');
+
+export function updateAuth() {
+    let serializedUser = localStorage.getItem('user');
+
+    if(serializedUser) {
+   
+        guestNavigation.style.display = 'none';
+        userNavigation.style.display = 'inline';
+    }else {
+        guestNavigation.style.display = 'inline';
+        userNavigation.style.display = 'none';
+    }
+}
+
+export function logout(){
+    localStorage.removeItem('user');
+
+    updateAuth()
+}
+export function getToken(){
+    let serializedUser = localStorage.getItem('user');
+
+    if(serializedUser) {
+        let user = JSON.parse(serializedUser);
+        return user.accessToken;
+    }
+}
+export function saveUser(user) {
+    localStorage.setItem('user', JSON.stringify(user));
+}
