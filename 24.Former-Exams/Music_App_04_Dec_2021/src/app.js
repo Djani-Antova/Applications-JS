757bdf93abb40d7691a8c4cb2a043c070cc1ca6a
djaniAntova <djaniantova@gmail.com>
2023-04-26 10:48:49 +0300

    Create app.js

---
+++ 24.Former-Exams/Music_App_04_Dec_2021/src/app.js
@@ -0,0 +1,33 @@
+import page from '../node_modules/page/page.mjs';
+import { authMiddleware } from './middlewares/authMiddleware.js';
+import { renderNavigationMiddleware, renderContentMiddleware } from './middlewares/renderMiddleware.js';
+import { catalogView } from './views/catalogView.js';
+import { createView } from './views/createView.js';
+import { deleteView } from './views/deleteView.js';
+import { detailsView } from './views/detailsView.js';
+import { editView } from './views/editView.js';
+import { homeView } from './views/homeView.js';
+import { loginView } from './views/loginView.js';
+import { logoutView } from './views/logoutView.js';
+import { registerView } from './views/registerView.js';
+import { searchView } from './views/searchView.js';
+
+
+page(authMiddleware);
+page(renderNavigationMiddleware);
+page(renderContentMiddleware);
+
+page('/', homeView);
+page('/login', loginView);
+page('/logout', logoutView);
+
+page('/register', registerView);
+page('/register', registerView);
+page('/catalog', catalogView);
+page('/create', createView);
+page('/serach', searchView);
+page('/albums/:albumId', detailsView);
+page('/albums/:albumId/edit', editView);
+page('/albums/:albumId/delete', deleteView);
+
+page.start();
