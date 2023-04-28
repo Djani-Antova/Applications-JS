af1582239431840a003a33897cabd180ea50e824
djaniAntova <djaniantova@gmail.com>
2023-04-28 13:12:23 +0300

    Update

---
+++ 24.Former-Exams/Games_Play_16_Aug_2021/src/views/edit.js
@@ -0,0 +1,58 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+
+import * as gamesService from '../api/games.js';
+import { createSubmitHandler } from '../util.js';
+
+const editTemplate = (game, onSubmit) => html `
+               <!-- Edit Page ( Only for the creator )-->
+               <section id="edit-page" class="auth">
+            <form @submit=${onSubmit} id="edit">
+                <div class="container">
+
+                    <h1>Edit Game</h1>
+                    <label for="leg-title">Legendary title:</label>
+                    <input type="text" id="title" name="title" .value=${game.title}>
+
+                    <label for="category">Category:</label>
+                    <input type="text" id="category" name="category" .value=${game.category}>
+
+                    <label for="levels">MaxLevel:</label>
+                    <input type="number" id="maxLevel" name="maxLevel" min="1" .value=${game.maxLevel}>
+
+                    <label for="game-img">Image:</label>
+                    <input type="text" id="imageUrl" name="imageUrl" .value=${game.imageUrl}>
+
+                    <label for="summary">Summary:</label>
+                    <textarea name="summary" id="summary" .value=${game.summary}></textarea>
+                    <input class="btn submit" type="submit" value="Edit Game">
+
+                </div>
+            </form>
+        </section>
+`;
+
+export async function editPage(ctx) {
+
+    const gameId = ctx.params.id;
+    const game = await gamesService.getById(gameId);
+
+    ctx.render(editTemplate(game, createSubmitHandler(ctx, onSubmit)))
+}
+
+async function onSubmit(ctx, data, event) {
+    const gameId = ctx.params.id;
+
+    if (Object.values(data).some(x => x == "")) {  //check if at least 1 element is empty
+        return alert ('All fields are required!')
+    }
+   
+    await gamesService.update(gameId, {        
+            title: data.title,
+            category: data.category,
+            maxLevel: data.maxLevel,
+            imageUrl: data.imageUrl,
+            summary: data.summary
+    });
+    event.target.reset() // empty input fields
+    ctx.page.redirect('/details/' + gameId)
+}
