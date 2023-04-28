498f94640ea828cbe9715d44d6f6aff38f9aea1d
djaniAntova <djaniantova@gmail.com>
2023-04-28 13:09:21 +0300

    Create comments.js

---
+++ 24.Former-Exams/Games_Play_16_Aug_2021/src/api/comments.js
@@ -0,0 +1,22 @@
+import * as api from './api.js';
+
+//import { getUserData,  getAccessToken } from "../util.js";
+
+const endpoints = {
+    byGameId: (gameId) => `/data/comments?where=gameId%3D%22${gameId}%22`,
+    post: '/data/comments'
+};
+
+    // {
+    // gameId,
+    // comment
+    // } 
+
+
+export async function getByGameId(gameId) {
+    return api.get(endpoints.byGameId(gameId))
+}
+
+export async function postComment(comment) {
+    return api.post(endpoints.post, comment)
+}
